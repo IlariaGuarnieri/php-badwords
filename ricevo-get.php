db3edac5582c72de63c19e9966947e3d751d5667
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-04-12 19:17:37 +0200

    [add] pagina GET

--- ricevo-get.php
+++ ricevo-get.php
@@ -1,29 +1,16 @@
-<?php
-$nome= 'Ilaria';
-$cognome= 'Guarnieri';
-
-$nome_completo= "$nome $cognome"
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css' integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==' crossorigin='anonymous'/>
 
-  <title>PHP Badwords</title>
+  <title>First PHP</title>
 </head>
-
 <body>
-<div class="container">
-<a href="ricevo-get.php"></a>
-
-<h5> Ciao sono <?php echo $nome_completo ?>Vai a ricevo Get</h5>
-</div>
+  <div class="container my-5">
+  <h1>ricevo GET</h1>
+  </div>
 </body>
-
 </html>
