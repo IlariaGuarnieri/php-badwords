9a2d1526e0550df83f58534a9ea97bf3ec96427b
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-04-12 18:53:09 +0200

    [add] bootstrap

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css' integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==' crossorigin='anonymous'/>
+
+  <title>PHP Badwords</title>
 </head>
+
 <body>
-  file dentro repo
+<div class="container">
+  
+</div>
 </body>
+
 </html>
